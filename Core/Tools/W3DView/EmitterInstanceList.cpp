8d466558c4de549ab9be7a02c196d49c600f3836
Bobby Battista <bobtista@gmail.com>
2025-11-22 15:36:28 -0500

    formatting change

--- Core/Tools/W3DView/EmitterInstanceList.cpp
+++ Core/Tools/W3DView/EmitterInstanceList.cpp
@@ -93,7 +93,8 @@ EmitterInstanceListClass::Add_Emitter (ParticleEmitterClass *emitter)
 		//
 		//	Add this emitter to the list and put a hold on its reference
 		//
-		if (emitter) emitter->Add_Ref();
+		if (emitter)
+			emitter->Add_Ref();
 		m_List.Add (emitter);
 	}
 
