778110ff04df8f3dd52fb95f896ad9d364002180
Bobby Battista <bobtista@gmail.com>
2026-01-03 19:34:05 -0800

    Replicate to generals

--- Generals/Code/GameEngine/Source/GameClient/Drawable.cpp
+++ Generals/Code/GameEngine/Source/GameClient/Drawable.cpp
@@ -1268,6 +1268,11 @@ void Drawable::calcPhysicsXformThrust(const Locomotor* locomotor, PhysicsXformIn
 	Real MAX_WOBBLE = locomotor->getMaxWobble();
 	Real MIN_WOBBLE = locomotor->getMinWobble();
 
+	// TheSuperHackers @tweak Wobble and thrust roll rates are now decoupled from the render update.
+	const Real timeScale = TheFramePacer->getActualLogicTimeScaleOverFpsRatio();
+	const Real scaledWobbleRate = WOBBLE_RATE * timeScale;
+	const Real scaledThrustRoll = THRUST_ROLL * timeScale;
+
 	//
 	// this is a kind of quick thrust implementation cause we need scud missiles to wobble *now*,
 	// we deal with just adjusting pitch, yaw, and roll just a little bit
@@ -1282,14 +1287,14 @@ void Drawable::calcPhysicsXformThrust(const Locomotor* locomotor, PhysicsXformIn
 			if (m_locoInfo->m_pitch < MAX_WOBBLE - WOBBLE_RATE * 2)
 			{
 
-				m_locoInfo->m_pitch += WOBBLE_RATE;
-				m_locoInfo->m_yaw += WOBBLE_RATE;
+				m_locoInfo->m_pitch += scaledWobbleRate;
+				m_locoInfo->m_yaw += scaledWobbleRate;
 			}
 			else
 			{
 
-				m_locoInfo->m_pitch += (WOBBLE_RATE / 2.0f);
-				m_locoInfo->m_yaw += (WOBBLE_RATE / 2.0f);
+				m_locoInfo->m_pitch += (scaledWobbleRate / 2.0f);
+				m_locoInfo->m_yaw += (scaledWobbleRate / 2.0f);
 			}
 
 			if (m_locoInfo->m_pitch >= MAX_WOBBLE)
@@ -1301,14 +1306,14 @@ void Drawable::calcPhysicsXformThrust(const Locomotor* locomotor, PhysicsXformIn
 			if (m_locoInfo->m_pitch >= MIN_WOBBLE + WOBBLE_RATE * 2.0f)
 			{
 
-				m_locoInfo->m_pitch -= WOBBLE_RATE;
-				m_locoInfo->m_yaw -= WOBBLE_RATE;
+				m_locoInfo->m_pitch -= scaledWobbleRate;
+				m_locoInfo->m_yaw -= scaledWobbleRate;
 			}
 			else
 			{
 
-				m_locoInfo->m_pitch -= (WOBBLE_RATE / 2.0f);
-				m_locoInfo->m_yaw -= (WOBBLE_RATE / 2.0f);
+				m_locoInfo->m_pitch -= (scaledWobbleRate / 2.0f);
+				m_locoInfo->m_yaw -= (scaledWobbleRate / 2.0f);
 			}
 			if (m_locoInfo->m_pitch <= MIN_WOBBLE)
 				m_locoInfo->m_wobble = 1.0f;
@@ -1321,7 +1326,7 @@ void Drawable::calcPhysicsXformThrust(const Locomotor* locomotor, PhysicsXformIn
 	if (THRUST_ROLL)
 	{
 
-		m_locoInfo->m_roll += THRUST_ROLL;
+		m_locoInfo->m_roll += scaledThrustRoll;
 		info.m_totalRoll = m_locoInfo->m_roll;
 	}
 }
