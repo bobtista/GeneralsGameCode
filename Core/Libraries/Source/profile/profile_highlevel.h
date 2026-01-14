3ad4cae351edb28d0fe844cf290d1bb2c4d08420
Bobby Battista <bobtista@gmail.com>
2026-01-14 10:28:37 -0500

    Update Core/Libraries/Source/profile/profile_highlevel.h
    Co-authored-by: Stubbjax <stubbjax02@hotmail.com>

--- Core/Libraries/Source/profile/profile_highlevel.h
+++ Core/Libraries/Source/profile/profile_highlevel.h
@@ -223,7 +223,7 @@ private:
 
   /** \internal
 
-    Undocumented default constructor. Initializes high level profiler.
+    Undocumented default constructor. Initializes high-level profiler.
     We can make this private as well so nobody accidentally tries to create
     another instance.
   */
