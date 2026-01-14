3622f9a220c0a1f08765855e99c422ba8ad630a0
Bobby Battista <bobtista@gmail.com>
2026-01-14 17:04:15 -0500

    fix(typos): Fix spelling errors in GeneralsMD/Code/GameEngine/Source (Client, Common) comments

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/IMEManager.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/IMEManager.cpp
@@ -97,7 +97,7 @@ class IMEManager : public IMEManagerInterface
 		virtual void					update( void );
 
 		virtual void					attach( GameWindow *window );		///< attach IME to specified window
-		virtual void					detatch( void );								///< detatch IME from current window
+		virtual void					detatch( void );								///< detach IME from current window
 		virtual void					enable( void );									///< Enable IME
 		virtual void					disable( void );								///< Disable IME
 		virtual Bool					isEnabled( void );							///< Is IME enabled
@@ -138,7 +138,7 @@ class IMEManager : public IMEManagerInterface
 
 		Int										m_result;												///< last IME message's winProc return code
 		GameWindow						*m_window;											///< window we are accepting input for
-		HIMC									m_context;											///< Imput Manager Context
+		HIMC									m_context;											///< Input Manager Context
 		HIMC									m_oldContext;										///< Previous IME comtext
 		Int										m_disabled;											///< IME disable count 0 = enabled
 		Bool									m_composing;										///< Are we currently composing a new string
@@ -152,7 +152,7 @@ class IMEManager : public IMEManagerInterface
 		Int										m_pageSize;											///< Number of candidate per page
 		Int										m_selectedIndex;								///< Index of the currently selected candidate
 		Int										m_candidateCount;								///< Total number of candidate strings
-		UnicodeString					*m_candidateString;							///< table of canidate strings
+		UnicodeString					*m_candidateString;							///< table of candidate strings
 		Bool									m_unicodeIME;										///< Is this an unicode IME
 		Int										m_compositionCharsDisplayed;		///< number of temporary composition characters displayed that need to be replaced with result string.
 
@@ -646,7 +646,7 @@ void IMEManager::attach( GameWindow *window )
 }
 
 //============================================================================
-// IMEManager::detatch
+// IMEManager::detach
 //============================================================================
 
 void IMEManager::detatch( void )
@@ -1099,7 +1099,7 @@ void IMEManager::updateCompositionString( void )
 
 	if ( m_context )
 	{
-		// try reading unicode directy
+		// try reading unicode directly
 		LONG result = ImmGetCompositionStringW( m_context, GCS_COMPSTR, m_compositionString, MAX_COMPSTRINGLEN );
 
 		if ( result >= 0 )
@@ -1165,7 +1165,7 @@ void IMEManager::getResultsString ( void )
 
 	if ( m_context )
 	{
-		// try reading unicode directy
+		// try reading unicode directly
 		LONG result = ImmGetCompositionStringW( m_context,  GCS_RESULTSTR, m_resultsString, MAX_COMPSTRINGLEN );
 
 		if ( result >= 0 )
@@ -1244,7 +1244,7 @@ void IMEManager::openCandidateList( Int candidateFlags )
 	{
 		return;
 	}
-	// first get lastest candidate list info
+	// first get latest candidate list info
 	updateCandidateList( candidateFlags );
   resizeCandidateWindow( m_pageSize );
 
