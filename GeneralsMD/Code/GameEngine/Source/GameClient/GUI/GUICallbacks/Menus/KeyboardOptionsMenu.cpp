869ac69c2cf8bab51e6f7bac1f62655f0eeeaae2
Bobby Battista <bobtista@gmail.com>
2026-01-18 18:44:13 -0600

    Update GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/KeyboardOptionsMenu.cpp
    Co-authored-by: Stubbjax <stubbjax02@hotmail.com>

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/KeyboardOptionsMenu.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/KeyboardOptionsMenu.cpp
@@ -1049,7 +1049,7 @@ WindowMsgHandledType KeyboardTextEntryInput( GameWindow *window, UnsignedInt msg
 																							0 );
 							return MSG_HANDLED;
 						}
-						//else check is modifiers are present
+						//else check if modifiers are present
 						else
 						{
 							char c = e->text->getText().getCharAt(e->text->getTextLength() - 1 );
