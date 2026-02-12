3c852dbf23c82d8e7e84a3a583430a885eda0bfd
Bobby Battista <bobtista@gmail.com>
2026-02-12 11:45:05 -0500

    fix(headless): Add static_cast for VC6 builds

--- GeneralsMD/Code/GameEngineDevice/Include/W3DDevice/GameClient/W3DGameClient.h
+++ GeneralsMD/Code/GameEngineDevice/Include/W3DDevice/GameClient/W3DGameClient.h
@@ -119,7 +119,7 @@ protected:
 #endif
 	/// factory for creating the TerrainVisual
 	// TheSuperHackers @fix bobtista 31/01/2026 Return dummy in headless mode
-	virtual TerrainVisual *createTerrainVisual( void ) { return TheGlobalData->m_headless ? NEW TerrainVisualDummy : NEW W3DTerrainVisual; }
+	virtual TerrainVisual *createTerrainVisual( void ) { return TheGlobalData->m_headless ? static_cast<TerrainVisual*>(NEW TerrainVisualDummy) : NEW W3DTerrainVisual; }
 
 	/// factory for creating the snow manager
 	virtual SnowManager *createSnowManager( void ) { return NEW W3DSnowManager; }
