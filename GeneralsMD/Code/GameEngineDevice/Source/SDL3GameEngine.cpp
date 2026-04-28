0aefe6c22bce43564e015044b3042b0300903ca5
bobtista <bobtista@gmail.com>
2026-04-28 11:45:43 +1000

    feat: add GeneralsMD OpenAL audio backend
    (cherry picked from commit 80667628177e2584083d4c50e9942f6abf3b79ec)

--- GeneralsMD/Code/GameEngineDevice/Source/SDL3GameEngine.cpp
+++ GeneralsMD/Code/GameEngineDevice/Source/SDL3GameEngine.cpp
@@ -27,6 +27,9 @@
 #include "GameClient/ParticleSys.h"
 #include "GameLogic/GameLogic.h"
 #include "GameNetwork/NetworkInterface.h"
+#if defined(SAGE_USE_OPENAL)
+#include "OpenALAudioDevice/OpenALAudioManager.h"
+#endif
 #include "SDL3Device/GameClient/SDL3Keyboard.h"
 #include "SDL3Device/GameClient/SDL3Mouse.h"
 #include "StdDevice/Common/StdBIGFileSystem.h"
@@ -232,7 +235,12 @@ WebBrowser *SDL3GameEngine::createWebBrowser()
 
 AudioManager *SDL3GameEngine::createAudioManager(Bool dummy)
 {
-	// TheSuperHackers @info bobtista 28/05/2026 Audio backend is wired up in the openal-audio topic; this stub here is intentional until that topic lands.
+#if defined(SAGE_USE_OPENAL)
+	if (!dummy)
+	{
+		return NEW OpenALAudioManager;
+	}
+#endif
 	return NULL;
 }
 
