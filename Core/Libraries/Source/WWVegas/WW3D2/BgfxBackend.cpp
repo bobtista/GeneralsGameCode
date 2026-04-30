e1c2f16471bccb1eb6ffe48f1b8764533f0b4d97
Bobby Battista <bobtista@gmail.com>
2026-04-30 16:22:20 +1000

    build(bgfx-mtl): add init pre-warm loop and bump shader profile to metal30-14

--- Core/Libraries/Source/WWVegas/WW3D2/BgfxBackend.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/BgfxBackend.cpp
@@ -2562,6 +2562,7 @@ void BgfxBackend::Initialize(void * hwnd, int /*width*/, int /*height*/)
                 bgfx::touch(allViews[i]);
             }
             bgfx::frame();
+<<<<<<< HEAD
             if (trace)
             {
                 std::fprintf(stderr, "[ggc] pre-warm pass %d done\n", pass);
