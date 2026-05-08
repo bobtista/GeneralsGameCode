86c343421f17d7692a8cd769a09f2f72ba065dd8
Bobby Battista <bobtista@gmail.com>
2026-05-08 22:18:57 +1000

    refactor(bgfx): extract render state cache

---
+++ Core/Libraries/Source/WWVegas/WW3D2/RenderStateCache.cpp
@@ -0,0 +1,67 @@
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+*/
+
+#include "RenderStateCache.h"
+
+#include <string.h>
+
+unsigned RenderStateCache::RenderStates[RenderStateCache::RENDER_STATE_COUNT];
+unsigned RenderStateCache::TextureStageStates[RenderStateCache::TEXTURE_STAGE_COUNT][RenderStateCache::TEXTURE_STAGE_STATE_COUNT];
+
+void RenderStateCache::Clear()
+{
+	memset(RenderStates, 0, sizeof(RenderStates));
+	memset(TextureStageStates, 0, sizeof(TextureStageStates));
+}
+
+void RenderStateCache::Invalidate()
+{
+	unsigned state;
+	for (state = 0; state < RENDER_STATE_COUNT; ++state) {
+		RenderStates[state] = INVALID_STATE_VALUE;
+	}
+
+	unsigned stage;
+	for (stage = 0; stage < TEXTURE_STAGE_COUNT; ++stage) {
+		for (state = 0; state < TEXTURE_STAGE_STATE_COUNT; ++state) {
+			TextureStageStates[stage][state] = INVALID_STATE_VALUE;
+		}
+	}
+}
+
+unsigned RenderStateCache::Get_Render_State(unsigned state)
+{
+	return RenderStates[state];
+}
+
+bool RenderStateCache::Set_Render_State(unsigned state, unsigned value)
+{
+	if (RenderStates[state] == value) {
+		return false;
+	}
+
+	RenderStates[state] = value;
+	return true;
+}
+
+unsigned RenderStateCache::Get_Texture_Stage_State(unsigned stage, unsigned state)
+{
+	return TextureStageStates[stage][state];
+}
+
+bool RenderStateCache::Set_Texture_Stage_State(unsigned stage, unsigned state, unsigned value)
+{
+	if (TextureStageStates[stage][state] == value) {
+		return false;
+	}
+
+	TextureStageStates[stage][state] = value;
+	return true;
+}
