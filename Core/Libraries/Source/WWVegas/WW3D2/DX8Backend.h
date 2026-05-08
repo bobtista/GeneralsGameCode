156392ab28b7454b13e4387669bbc5cff20a8a6a
Bobby Battista <bobtista@gmail.com>
2026-05-09 09:34:01 +1000

    refactor(render): route water transforms
    (cherry picked from commit 43f04ad3a60d1d9049f846b0098846c1f976350e)

--- Core/Libraries/Source/WWVegas/WW3D2/DX8Backend.h
+++ Core/Libraries/Source/WWVegas/WW3D2/DX8Backend.h
@@ -136,6 +136,7 @@ public:
     virtual void Set_Color_Write_Mask(unsigned mask) override;
     virtual void Set_Lighting_Enable(bool enable);
     virtual void Set_Texture_Factor(unsigned argb);
+    virtual void Set_Texture_Transform(unsigned stage, const Matrix4x4 & matrix) override;
     virtual void Set_Texture_Stage_State(unsigned stage, unsigned state, unsigned value) override;
     virtual CullMode Get_Cull_Mode() const override;
     virtual void Set_Cull_Mode(CullMode mode) override;
