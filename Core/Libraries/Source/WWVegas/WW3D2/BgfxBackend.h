f7fec9256c2292d4c26b01b899a0978f9b54cce2
Bobby Battista <bobtista@gmail.com>
2026-05-08 22:39:14 +1000

    refactor(bgfx): cache material and lights locally
    (cherry picked from commit 5866b70b0b8b4b328c28c69fb0e66510249c1450)

--- Core/Libraries/Source/WWVegas/WW3D2/BgfxBackend.h
+++ Core/Libraries/Source/WWVegas/WW3D2/BgfxBackend.h
@@ -139,6 +139,8 @@ public:
     virtual void Set_Material(const VertexMaterialClass * material) override;
     virtual void Set_Texture(unsigned int stage, TextureBaseClass * texture) override;
     virtual void Bind_Texture_Immediate(unsigned int stage, TextureBaseClass * texture) override;
+    virtual void Set_Light(unsigned int index, const LightClass & light) override;
+    virtual void Clear_Light(unsigned int index) override;
     virtual void Set_Light_Environment(LightEnvironmentClass * light_env) override;
     virtual void Set_Ambient(const Vector3 & color) override;
     virtual void Set_Fog(bool enable, const Vector3 & color, float start, float end) override;
