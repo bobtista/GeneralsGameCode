b490cc382cd30ad787e7f3cdc2ac4e89050ca245
Bobby Battista <bobtista@gmail.com>
2026-05-09 12:45:15 +1000

    refactor(render): query color write support through backend
    (cherry picked from commit a0584f9805f0d276265961c576bd740b5c2944c8)

--- Core/Libraries/Source/WWVegas/WW3D2/IRenderBackend.h
+++ Core/Libraries/Source/WWVegas/WW3D2/IRenderBackend.h
@@ -672,6 +672,7 @@ public:
     virtual void Set_Depth_Test_Enable(bool enable) {}
     virtual void Set_Depth_Write_Enable(bool enable) {}
     virtual void Set_Depth_Func(CompareFunc func) {}
+    virtual bool Supports_Color_Write_Mask() const { return true; }
     virtual unsigned Get_Color_Write_Mask() const { return RB_COLOR_RGBA; }
     virtual void Set_Color_Write_Mask(unsigned mask) {}
     virtual void Set_Lighting_Enable(bool enable) {}
