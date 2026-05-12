39a1bce552dac37c98149b339968335d9b92ff47
Bobby Battista <bobtista@gmail.com>
2026-05-12 19:36:51 +1000

    refactor(render): move state cache storage

--- Core/Libraries/Source/WWVegas/WW3D2/RenderStateCache.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/RenderStateCache.cpp
@@ -10,98 +10,42 @@
 
 #include "RenderStateCache.h"
 
-#include <string.h>
-
-unsigned RenderStateCache::RenderStates[RenderStateCache::RENDER_STATE_COUNT];
-unsigned RenderStateCache::TextureStageStates[RenderStateCache::TEXTURE_STAGE_COUNT][RenderStateCache::TEXTURE_STAGE_STATE_COUNT];
-D3DMATRIX RenderStateCache::Transforms[RenderStateCache::TRANSFORM_COUNT];
-
 void RenderStateCache::Clear()
 {
-	memset(RenderStates, 0, sizeof(RenderStates));
-	memset(TextureStageStates, 0, sizeof(TextureStageStates));
-	memset(Transforms, 0, sizeof(Transforms));
+	FixedFunctionState::Clear_Cached_State();
 }
 
 void RenderStateCache::Invalidate()
 {
-	unsigned state;
-	for (state = 0; state < RENDER_STATE_COUNT; ++state) {
-		RenderStates[state] = INVALID_STATE_VALUE;
-	}
-
-	unsigned stage;
-	for (stage = 0; stage < TEXTURE_STAGE_COUNT; ++stage) {
-		for (state = 0; state < TEXTURE_STAGE_STATE_COUNT; ++state) {
-			TextureStageStates[stage][state] = INVALID_STATE_VALUE;
-		}
-	}
-
-	memset(Transforms, 0, sizeof(Transforms));
+	FixedFunctionState::Invalidate_Cached_State();
 }
 
 unsigned RenderStateCache::Get_Render_State(unsigned state)
 {
-	if (state >= RENDER_STATE_COUNT) {
-		return INVALID_STATE_VALUE;
-	}
-
-	return RenderStates[state];
+	return FixedFunctionState::Cached_Render_State(state);
 }
 
 bool RenderStateCache::Set_Render_State(unsigned state, unsigned value)
 {
-	if (state >= RENDER_STATE_COUNT) {
-		return false;
-	}
-
-	if (RenderStates[state] == value) {
-		return false;
-	}
-
-	RenderStates[state] = value;
-	return true;
+	return FixedFunctionState::Set_Cached_Render_State(state, value);
 }
 
 unsigned RenderStateCache::Get_Texture_Stage_State(unsigned stage, unsigned state)
 {
-	if (stage >= TEXTURE_STAGE_COUNT || state >= TEXTURE_STAGE_STATE_COUNT) {
-		return INVALID_STATE_VALUE;
-	}
-
-	return TextureStageStates[stage][state];
+	return FixedFunctionState::Cached_Texture_Stage_State(stage, state);
 }
 
 bool RenderStateCache::Set_Texture_Stage_State(unsigned stage, unsigned state, unsigned value)
 {
-	if (stage >= TEXTURE_STAGE_COUNT || state >= TEXTURE_STAGE_STATE_COUNT) {
-		return false;
-	}
-
-	if (TextureStageStates[stage][state] == value) {
-		return false;
-	}
-
-	TextureStageStates[stage][state] = value;
-	return true;
+	return FixedFunctionState::Set_Cached_Texture_Stage_State(stage, state, value);
 }
 
 void RenderStateCache::Get_Transform(unsigned transform, D3DMATRIX & matrix)
 {
-	if (transform >= TRANSFORM_COUNT) {
-		memset(&matrix, 0, sizeof(matrix));
-		return;
-	}
-
-	matrix = Transforms[transform];
+	FixedFunctionState::Cached_Transform(transform, matrix);
 }
 
 bool RenderStateCache::Set_Transform(unsigned transform, const D3DMATRIX & matrix)
 {
-	if (transform >= TRANSFORM_COUNT) {
-		return false;
-	}
-
-	Transforms[transform] = matrix;
-	return true;
+	return FixedFunctionState::Set_Cached_Transform(transform, matrix);
 }
