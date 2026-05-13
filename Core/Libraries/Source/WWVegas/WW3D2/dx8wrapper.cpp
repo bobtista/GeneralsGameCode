9ea92e46cfd2008ca17937e80a9368184fbf599a
Bobby Battista <bobtista@gmail.com>
2026-05-14 08:39:54 +1000

    refactor(dx8wrapper): guard cache invalidation device reset
    (cherry picked from commit 68120a319cf5e70dd056d0e7d904fcf97d2cfd9e)

--- Core/Libraries/Source/WWVegas/WW3D2/dx8wrapper.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/dx8wrapper.cpp
@@ -534,6 +534,7 @@ void DX8Wrapper::Invalidate_Cached_Render_States()
 	FixedFunctionState::Changed_Mask()=0;
 	RenderStateCache::Invalidate();
 
+#if !defined(GGC_BGFX_STANDALONE)
 	int a;
 	for (a=0;a<MAX_TEXTURE_STAGES;++a)
 	{
@@ -542,6 +543,7 @@ void DX8Wrapper::Invalidate_Cached_Render_States()
 		if (_Get_D3D_Device8())
 			_Get_D3D_Device8()->SetTexture(a,nullptr);
 	}
+#endif
 	FixedFunctionState::Release_Raw_Textures();
 
 	ShaderClass::Invalidate();
