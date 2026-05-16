9ae72e82288589c16e3188cc9d05d4bad27f704c
Bobby Battista <bobtista@gmail.com>
2026-05-16 15:49:56 +1000

    fix(render2d): assign hotKeyPosY in centered builder and remove double AddRef

--- Core/Libraries/Source/WWVegas/WW3D2/render2dsentence.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/render2dsentence.cpp
@@ -746,7 +746,6 @@ Render2DSentenceClass::Allocate_New_Surface (const WCHAR *text, bool justCalcExt
 		//
 		CurSurface = NEW_REF (SurfaceClass, (CurrTextureSize, CurrTextureSize, WW3D_FORMAT_A4R4G4B4));
 		WWASSERT (CurSurface != nullptr);
-		CurSurface->Add_Ref ();
 
 		//
 		//	Add this surface to our list
@@ -910,6 +909,7 @@ void	Render2DSentenceClass::Build_Sentence_Centered (const WCHAR *text, int *hkX
 		{
 			calcHotKeyX = false;
 			hotKeyPosX = Cursor.X + notCenteredHotkeyX;
+			hotKeyPosY = Cursor.Y;
 		}
 
 		for(int i = 0; i <= charCount; i++) {
