c3d111772541730914ca8c476ab7324bb8837044
Bobby Battista <bobtista@gmail.com>
2026-05-22 00:02:19 +1000

    Upload bgfx shroud without texture surface copy

--- GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DShroud.cpp
+++ GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DShroud.cpp
@@ -709,12 +709,6 @@ void W3DShroud::render(CameraClass *cam)
 		m_pDstTexture->Get_Filter().Set_Min_Filter(m_shroudFilter);
 	}
 
-	//Update video memory texture with sysmem copy
-	SurfaceClass* pDestSurface;
-	{
-		pDestSurface=m_pDstTexture->Get_Surface_Level(0);
-	}
-
 	RECT	srcRect;
 	POINT	dstPoint={1,1};	//first row/column is reserved for border.
 
@@ -733,20 +727,28 @@ void W3DShroud::render(CameraClass *cam)
 		//color in order to keep map border in the state we want.
 		m_clearDstTexture=FALSE;
 
+#if !defined(GGC_RENDER_BACKEND_BGFX)
+		SurfaceClass *pDestSurface=m_pDstTexture->Get_Surface_Level(0);
 		fillBorderShroudData(m_boderShroudLevel, pDestSurface);
+		REF_PTR_RELEASE (pDestSurface);
+#endif
 	}
 
+#if !defined(GGC_RENDER_BACKEND_BGFX)
 	{
+		SurfaceClass *pDestSurface=m_pDstTexture->Get_Surface_Level(0);
 		//USE_PERF_TIMER(shroudCopy)
 		// TheSuperHackers @refactor bobtista 10/04/2026 Phase 3C: SurfaceClass::Copy
 		// in place of _Copy_DX8_Rects.
 		pDestSurface->Copy(
-				dstPoint.x, dstPoint.y,
-				srcRect.left, srcRect.top,
-				srcRect.right - srcRect.left,
-				srcRect.bottom - srcRect.top,
-				m_pSrcTexture);
+			dstPoint.x, dstPoint.y,
+			srcRect.left, srcRect.top,
+			srcRect.right - srcRect.left,
+			srcRect.bottom - srcRect.top,
+			m_pSrcTexture);
+		REF_PTR_RELEASE (pDestSurface);
 	}
+#endif
 
 	// TheSuperHackers @feature bobtista 17/04/2026 Push shroud pixel data to
 	// the bgfx backend so it can mirror the POOL_DEFAULT destination texture.
@@ -829,8 +831,6 @@ void W3DShroud::render(CameraClass *cam)
 			m_srcTexturePitch,
 			srcDesc.Format);
 	}
-
-	REF_PTR_RELEASE (pDestSurface);
 }
 
 #define FOG_INTERPOLATION_RATE	(255.0f/1000.0f)	//take one second to go from black to fully lit.
