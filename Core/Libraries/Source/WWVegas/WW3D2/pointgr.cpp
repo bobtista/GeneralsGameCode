1ba3bb3b29d92619e2a626930c230b711bbfd71d
Bobby Battista <bobtista@gmail.com>
2026-05-21 15:05:22 +1000

    Use neutral index buffers for point groups
    (cherry picked from commit 533a94a5cb6d37a6bc2ecfbbca45459730f7da32)

--- Core/Libraries/Source/WWVegas/WW3D2/pointgr.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/pointgr.cpp
@@ -89,6 +89,7 @@
 #include "sortingrenderer.h"
 #include "RenderBackend.h"
 #include "IRenderBackend.h"
+#include "renderbufferclasses.h"
 
 #include <cmath>
 #include <cstdio>
@@ -131,14 +132,14 @@ VectorClass<Vector3>			VertexLoc;		// camera-space vertex locations
 VectorClass<Vector4>			VertexDiffuse;	// vertex diffuse/alpha colors
 VectorClass<Vector2>			VertexUV;		// vertex texture coords
 
-// Some DX 8 variables
+// Some render buffer constants
 #define MAX_VB_SIZE			2048
 #define MAX_TRI_POINTS		MAX_VB_SIZE/3
 #define MAX_TRI_IB_SIZE		3*MAX_TRI_POINTS
 #define MAX_QUAD_POINTS		MAX_VB_SIZE/4
 #define MAX_QUAD_IB_SIZE	6*MAX_QUAD_POINTS
 
-DX8IndexBufferClass			*Tris, *Quads;						// Index buffers.
+RenderIndexBufferClass		*Tris, *Quads;					// Index buffers.
 SortingIndexBufferClass		*SortingTris, *SortingQuads;	// Sorting index buffers.
 
 /**************************************************************************
@@ -1571,21 +1572,21 @@ void PointGroupClass::_Init()
 	}
 
 	// Create the IBs
-	Tris=NEW_REF(DX8IndexBufferClass,(MAX_TRI_IB_SIZE));
-	Quads=NEW_REF(DX8IndexBufferClass,(MAX_QUAD_IB_SIZE));
+	Tris=NEW_REF(RenderIndexBufferClass,(MAX_TRI_IB_SIZE));
+	Quads=NEW_REF(RenderIndexBufferClass,(MAX_QUAD_IB_SIZE));
 	SortingTris=NEW_REF(SortingIndexBufferClass,(MAX_TRI_IB_SIZE));
 	SortingQuads=NEW_REF(SortingIndexBufferClass,(MAX_QUAD_IB_SIZE));
 
 	// Fill up the IBs
 	{
-		DX8IndexBufferClass::WriteLockClass locktris(Tris);
+		RenderIndexBufferClass::WriteLockClass locktris(Tris);
 		unsigned short *ib=locktris.Get_Index_Array();
 		for (i=0; i<MAX_TRI_IB_SIZE; i++) ib[i]=(unsigned short) i;
 	}
 
 	{
 		unsigned short vert=0;
-		DX8IndexBufferClass::WriteLockClass lockquads(Quads);
+		RenderIndexBufferClass::WriteLockClass lockquads(Quads);
 		unsigned short *ib=lockquads.Get_Index_Array();
 		vert=0;
 		for (i=0; i<MAX_QUAD_IB_SIZE; i+=6)
