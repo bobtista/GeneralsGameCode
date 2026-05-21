0f75f386134d6197bb139ae8ff00d3464deeb1dd
Bobby Battista <bobtista@gmail.com>
2026-05-22 07:38:08 +1000

    Guard standalone legacy surface attachment
    (cherry picked from commit 838fb66f5e60b1d649bebbb164f1d74bc93a870b)

--- Core/Libraries/Source/WWVegas/WW3D2/surfaceclass.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/surfaceclass.cpp
@@ -258,7 +258,13 @@ SurfaceClass::~SurfaceClass()
 		TextureOwner = nullptr;
 	}
 	if (D3DSurface) {
+#if defined(GGC_BGFX_STANDALONE)
+		WWASSERT_PRINT(
+			false,
+			"SurfaceClass::~SurfaceClass: standalone bgfx cannot release fake-D3D surfaces");
+#else
 		LEGACY_SURFACE->Release();
+#endif
 		D3DSurface = nullptr;
 	}
 }
@@ -1041,6 +1047,13 @@ void SurfaceClass::Get_Pixel(Vector3 &rgb, int x, int y, LockedSurfacePtr pBits,
 void SurfaceClass::Attach_Legacy_Surface(void *surface)
 {
 	Detach ();
+#if defined(GGC_BGFX_STANDALONE)
+	WWASSERT_PRINT(
+		surface == nullptr,
+		"SurfaceClass::Attach_Legacy_Surface: standalone bgfx cannot attach fake-D3D surfaces");
+	D3DSurface = nullptr;
+	return;
+#else
 	D3DSurface = static_cast<LegacySurface *>(surface);
 
 	//
@@ -1050,6 +1063,7 @@ void SurfaceClass::Attach_Legacy_Surface(void *surface)
 		LEGACY_SURFACE->AddRef ();
 		Update_Description_From_Legacy_Surface();
 	}
+#endif
 }
 
 void SurfaceClass::Update_Description_From_Legacy_Surface()
