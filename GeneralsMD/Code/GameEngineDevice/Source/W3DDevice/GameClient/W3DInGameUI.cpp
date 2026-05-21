6b96ce0cbf9230e816e9134b9d1c2b8278593863
Bobby Battista <bobtista@gmail.com>
2026-05-21 23:05:51 +1000

    Guard DX8 buffer includes in bgfx runtime paths
    (cherry picked from commit 7777e5ef4dd91441a796240a045b24f035d2c20e)

--- GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DInGameUI.cpp
+++ GeneralsMD/Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DInGameUI.cpp
@@ -57,8 +57,10 @@
 
 #ifdef RTS_DEBUG
 #include "W3DDevice/GameClient/HeightMap.h"
+#if !defined(GGC_BGFX_STANDALONE)
 #include "WW3D2/dx8indexbuffer.h"
 #include "WW3D2/dx8vertexbuffer.h"
+#endif
 #include "WW3D2/RenderBackend.h"
 #include "WW3D2/renderbufferclasses.h"
 #include "WW3D2/vertmaterial.h"
