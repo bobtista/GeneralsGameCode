64d17ee624836b85dbd3ece30cba150775a10bad
Bobby Battista <bobtista@gmail.com>
2026-05-21 19:12:42 +1000

    Guard water texture interop include to DX8
    (cherry picked from commit 1591059b2ec0c20a8c68e0e3d31d6e5b48a347d9)

--- Core/GameEngineDevice/Source/W3DDevice/GameClient/Water/W3DWater.cpp
+++ Core/GameEngineDevice/Source/W3DDevice/GameClient/Water/W3DWater.cpp
@@ -38,7 +38,6 @@
 #include "W3DDevice/GameClient/W3DWaterTracks.h"
 #include "W3DDevice/GameClient/W3DAssetManager.h"
 #include "texture.h"
-#include "dx8textureinterop.h"
 #include "assetmgr.h"
 #include "rinfo.h"
 #include "camera.h"
