1c70b7e4d55d08f627ad3b8e8e5ac1d432eb4b82
Bobby Battista <bobtista@gmail.com>
2026-05-22 16:45:50 +1000

    Add -srgb command line argument for gamma-correct rendering
    (cherry picked from commit c301033e8960d2e53468cecee11ba374cc145721)

--- GeneralsMD/Code/GameEngine/Source/Common/CommandLine.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/CommandLine.cpp
@@ -1100,6 +1100,12 @@ Int parseMsaa(char *args[], int num)
 	return 1;
 }
 
+Int parseSrgb(char *args[], int num)
+{
+	setenv("GGC_BGFX_SRGB", "1", 1);
+	return 1;
+}
+
 Int parseLogFrameTimes(char *args[], int num)
 {
 	if (TheFramePacer != nullptr)
@@ -1316,6 +1322,7 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-noFPSLimit", parseNoFPSLimit },
 	{ "-maxRenderFPS", parseMaxRenderFPS },
 	{ "-msaa", parseMsaa },
+	{ "-srgb", parseSrgb },
 	{ "-logFrameTimes", parseLogFrameTimes },
 	{ "-logBgfxStats", parseLogBgfxStats },
 	{ "-bgfxNoSceneFramebuffer", parseBgfxNoSceneFramebuffer },
