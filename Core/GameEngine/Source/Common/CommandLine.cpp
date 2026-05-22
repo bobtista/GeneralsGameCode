04101fb9aa8aa0e7b1abfe891ce87a9daa219da0
bobtista <bobtista@gmail.com>
2026-05-22 16:45:50 +1000

    feat(bgfx): add the -srgb flag for gamma-correct rendering

--- Core/GameEngine/Source/Common/CommandLine.cpp
+++ Core/GameEngine/Source/Common/CommandLine.cpp
@@ -1103,6 +1103,12 @@ Int parseMsaa(char *args[], int num)
 	return 1;
 }
 
+Int parseSrgb(char *args[], int num)
+{
+	setenv("GGC_BGFX_SRGB", "1", 1);
+	return 1;
+}
+
 Int parseLogFrameTimes(char *args[], int num)
 {
 	if (TheFramePacer != nullptr)
@@ -1323,6 +1329,7 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-noFPSLimit", parseNoFPSLimit },
 	{ "-maxRenderFPS", parseMaxRenderFPS },
 	{ "-msaa", parseMsaa },
+	{ "-srgb", parseSrgb },
 	{ "-logFrameTimes", parseLogFrameTimes },
 	{ "-logBgfxStats", parseLogBgfxStats },
 	{ "-bgfxNoSceneFramebuffer", parseBgfxNoSceneFramebuffer },
