373034036bf581a84d531224e3a4190750ef77fd
xezon <4720891+xezon@users.noreply.github.com>
2026-05-23 17:17:53 +0200

    Rename Fabsf_Origin to Fabsf_Legacy

--- Core/Libraries/Source/WWVegas/WWMath/wwmath.h
+++ Core/Libraries/Source/WWVegas/WWMath/wwmath.h
@@ -145,7 +145,7 @@ public:
 	static WWINLINE double Tan(double x) { return gm_tan(x); }
 	static WWINLINE float Tanf(float x) { return gm_tanf(x); }
 	static WWINLINE double Fabs_Origin(double x) { return gm_fabs(x); }
-	static WWINLINE float Fabsf_Origin(float x) { return gm_fabsf(x); }
+	static WWINLINE float Fabsf_Legacy(float x) { return gm_fabsf(x); }
 	static WWINLINE double Pow(double x, double y) { return gm_pow(x, y); }
 	static WWINLINE float Powf(float x, float y) { return gm_powf(x, y); }
 	static WWINLINE double Ceil(double x) { return gm_ceil(x); }
@@ -174,7 +174,7 @@ public:
 	static WWINLINE double Tan(double x) { return tan(x); }
 	static WWINLINE float Tanf(float x) { return tanf(x); }
 	static WWINLINE double Fabs_Origin(double x) { return fabs(x); }
-	static WWINLINE float Fabsf_Origin(float x) { return fabsf(x); }
+	static WWINLINE float Fabsf_Legacy(float x) { return fabsf(x); }
 	static WWINLINE double Pow(double x, double y) { return pow(x, y); }
 	static WWINLINE float Powf(float x, float y) { return powf(x, y); }
 	static WWINLINE double Ceil(double x) { return ceil(x); }
