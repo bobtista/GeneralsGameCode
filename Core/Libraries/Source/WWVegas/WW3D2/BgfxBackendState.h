8c26b12237c4cc329b5e267ad2fd12c7dc6bae38
Bobby Battista <bobtista@gmail.com>
2026-05-24 10:22:35 +1000

    Fix bgfx texture sampler state preservation
    (cherry picked from commit cc0d13f2693636fc2d28a7e67b77c714d32ad930)

--- Core/Libraries/Source/WWVegas/WW3D2/BgfxBackendState.h
+++ Core/Libraries/Source/WWVegas/WW3D2/BgfxBackendState.h
@@ -335,7 +335,6 @@ struct BgfxDraw
     float shroudOffset[4]     = { 0.0f, 0.0f, 0.0f, 0.0f };
     float shroudScale[4]      = { 0.0f, 0.0f, 1.0f, 1.0f };
     float shroudTextureParams[4] = { 0.0f, 0.0f, 1.0f, 1.0f };
-    // .w optionally clamps shroud multiplicative passes above full black.
     float objectShroudDim[4]  = { 1.0f, 0.0f, 0.0f, 0.0f };
     bool shroudTextureParamsValid = false;
     bool delayedObjectShroudPass = false;
