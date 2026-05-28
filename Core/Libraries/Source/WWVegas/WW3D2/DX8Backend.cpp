3c7b9a80727e83fe2cfeb07dd68264a408d12e49
Bobby Battista <bobtista@gmail.com>
2026-05-28 20:00:31 +1000

    style(DX8Backend): add braces to single-line if bodies in texture argument setters

--- Core/Libraries/Source/WWVegas/WW3D2/DX8Backend.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/DX8Backend.cpp
@@ -1617,7 +1617,9 @@ void DX8Backend::Set_Texture_Color_Argument(unsigned stage,
         D3DTSS_COLORARG2,
     };
     if (argument_index >= sizeof(states) / sizeof(states[0]))
+    {
         return;
+    }
 
     DX8Wrapper::Set_DX8_Texture_Stage_State(stage, states[argument_index], static_cast<DWORD>(arg));
 }
@@ -1632,7 +1634,9 @@ void DX8Backend::Set_Texture_Alpha_Argument(unsigned stage,
         D3DTSS_ALPHAARG2,
     };
     if (argument_index >= sizeof(states) / sizeof(states[0]))
+    {
         return;
+    }
 
     DX8Wrapper::Set_DX8_Texture_Stage_State(stage, states[argument_index], static_cast<DWORD>(arg));
 }
