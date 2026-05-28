e559e2b631502ff4d250c6fc86bc44b9024bc072
Bobby Battista <bobtista@gmail.com>
2026-05-28 15:35:32 +1000

    incorporate phase4 drift into bgfx-texture-decoupling

--- Core/Libraries/Source/WWVegas/WW3D2/TextureResourceManager.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/TextureResourceManager.cpp
@@ -18,14 +18,7 @@
 
 #include "TextureResourceManager.h"
 
-#include "dx8textureinterop.h"
-
 TextureTrackerList TextureResourceManagerClass::Managed_Textures;
-
-void TextureTrackerClass::Release()
-{
-	Set_Legacy_Base_Texture(*Texture, nullptr);
-}
 
 void TextureResourceManagerClass::Shutdown()
 {
