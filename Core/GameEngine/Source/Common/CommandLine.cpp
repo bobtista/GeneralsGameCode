371ee1ad464399cbd5483e312c9bde4426319b7e
Bobby Battista <bobtista@gmail.com>
2026-06-10 10:18:20 +1000

    test: dump per-function double-precision math bits to pinpoint diverging gm_ function
    (cherry picked from commit e7a50b4e223f5ea4a333b40c42a90edad9f90483)

--- Core/GameEngine/Source/Common/CommandLine.cpp
+++ Core/GameEngine/Source/Common/CommandLine.cpp
@@ -407,6 +407,7 @@ Int parseMathCrcCheck(char *args[], int)
 	const UnsignedInt crcDouble = SimulationMathCrc::calculateDouble();
 	printf("SimulationMathCrc = %08X\n", crc);
 	printf("SimulationMathCrcDouble = %08X\n", crcDouble);
+	SimulationMathCrc::dumpDoubleProbe();
 	fflush(stdout);
 	DEBUG_LOG(("SimulationMathCrc = %08X", crc));
 	DEBUG_LOG(("SimulationMathCrcDouble = %08X", crcDouble));
