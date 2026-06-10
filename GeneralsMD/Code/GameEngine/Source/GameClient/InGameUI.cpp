c0d4a49de4896899c71acddc7e778b8f91e60bec
Bobby Battista <bobtista@gmail.com>
2026-06-10 14:19:32 +1000

    bugfix: keep MSVC %s/%c wide-printf semantics on non-Windows so format args aren't truncated to one char

--- GeneralsMD/Code/GameEngine/Source/GameClient/InGameUI.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/InGameUI.cpp
@@ -2301,7 +2301,7 @@ void InGameUI::message( AsciiString stringManagerLabel, ... )
 	va_list args;
 	va_start( args, stringManagerLabel );
 	WideChar buf[ UnicodeString::MAX_FORMAT_BUF_LEN ];
-	int result = vswprintf(buf, sizeof( buf )/sizeof( WideChar ), stringManagerString.str(), args );
+	int result = formatStringW(buf, sizeof( buf )/sizeof( WideChar ), stringManagerString.str(), args );
 	va_end(args);
 
 	if( result >= 0 )
@@ -2342,7 +2342,7 @@ void InGameUI::message( UnicodeString format, ... )
 	va_list args;
 	va_start( args, format );
 	WideChar buf[ UnicodeString::MAX_FORMAT_BUF_LEN ];
-	int result = vswprintf(buf, sizeof( buf )/sizeof( WideChar ), format.str(), args );
+	int result = formatStringW(buf, sizeof( buf )/sizeof( WideChar ), format.str(), args );
 	va_end(args);
 
 	if( result >= 0 )
@@ -2369,7 +2369,7 @@ void InGameUI::messageColor( const RGBColor *rgbColor, UnicodeString format, ...
 	va_list args;
 	va_start( args, format );
 	WideChar buf[ UnicodeString::MAX_FORMAT_BUF_LEN ];
-	int result = vswprintf(buf, sizeof( buf )/sizeof( WideChar ), format.str(), args );
+	int result = formatStringW(buf, sizeof( buf )/sizeof( WideChar ), format.str(), args );
 	va_end(args);
 
 	if( result >= 0 )
