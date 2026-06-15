207a93ae854acb74920734f9b46fd6ad4f3c97cd
Bobby Battista <bobtista@gmail.com>
2026-06-16 00:43:18 +0700

    feat(bgfx): add fresnel rim lighting with alpha-test gate and soft highlight rolloff

--- GeneralsMD/Code/GameEngine/Include/Common/GlobalData.h
+++ GeneralsMD/Code/GameEngine/Include/Common/GlobalData.h
@@ -177,6 +177,9 @@ public:
 	Real m_bgfxRenderScale;
 	Bool m_bgfxSpecular;
 	Real m_bgfxSpecularStrength;
+	Bool m_bgfxRimLight;
+	Real m_bgfxRimStrength;
+	Real m_bgfxRimPower;
 	Bool m_bgfxSoftParticles;
 	Real m_bgfxSoftParticleFadeScale;
 	Real m_bgfxHeatHazeOpacityScale;
