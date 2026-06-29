05451b7b05b9d2e5c7a72730ceec41c195e2f8de
Bobby Battista <bobtista@gmail.com>
2026-06-29 13:21:27 -0400

    docs(drawable): Document xfer version 9 in version history

--- GeneralsMD/Code/GameEngine/Source/GameClient/Drawable.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/Drawable.cpp
@@ -4818,6 +4818,7 @@ void Drawable::xferDrawableModules(Xfer* xfer)
  * 6: Added m_ambientSoundEnabledFromScript flag
  * 7: Save the customize ambient sound info
  * 8: TheSuperHackers @bugfix Removed m_prevTintStatus because loading its value is unnecessary and undesirable
+ * 9: TheSuperHackers @tweak Changed m_timeElapsedFade and wheel airborne timers from integer to Real for frame-rate independent timing
  */
 // ------------------------------------------------------------------------------------------------
 void Drawable::xfer(Xfer* xfer)
