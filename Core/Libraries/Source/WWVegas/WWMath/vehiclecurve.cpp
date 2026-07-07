xezon/CnC_Generals_Zero_Hour

Rename Cos, Sin to Cosf, Sinf

Core/Libraries/Source/WWVegas/WWMath/vehiclecurve.cpp
@@ -208,8 +208,8 @@ Find_Turn_Arc
 	//
 	//	Find the point on the circle at this angle
 	//
-	arc_center->X = curr_pt.X + (radius * ::WWMath::Cos (avg_angle));
-	arc_center->Y = curr_pt.Y + (radius * ::WWMath::Sin (avg_angle));
+	arc_center->X = curr_pt.X + (radius * ::WWMath::Cosf (avg_angle));
+	arc_center->Y = curr_pt.Y + (radius * ::WWMath::Sinf (avg_angle));
 	arc_center->Z = curr_pt.Z;
 
 	//
@@ -378,12 +378,12 @@ VehicleCurveClass::Update_Arc_List ()
 		//	Determine at what points these angles intersect the arc
 		//
 		Vector3 point_in (0, 0, 0);
-		point_in.X = arc_center.X + (m_Radius * ::WWMath::Sin (point_angle + angle_in_delta));
-		point_in.Y = arc_center.Y + (m_Radius * -::WWMath::Cos (point_angle + angle_in_delta));
+		point_in.X = arc_center.X + (m_Radius * ::WWMath::Sinf (point_angle + angle_in_delta));
+		point_in.Y = arc_center.Y + (m_Radius * -::WWMath::Cosf (point_angle + angle_in_delta));
 
 		Vector3 point_out (0, 0, 0);
-		point_out.X = arc_center.X + (m_Radius * ::WWMath::Sin (point_angle + angle_out_delta));
-		point_out.Y = arc_center.Y + (m_Radius * -::WWMath::Cos (point_angle + angle_out_delta));
+		point_out.X = arc_center.X + (m_Radius * ::WWMath::Sinf (point_angle + angle_out_delta));
+		point_out.Y = arc_center.Y + (m_Radius * -::WWMath::Cosf (point_angle + angle_out_delta));
 
 		//
 		//	Sanity check to ensure the vehicle doesn't try to go the long way around the
@@ -513,8 +513,8 @@ VehicleCurveClass::Evaluate (float time, Vector3 *set_val)
 		//float angle = arc_info0.point_angle + (arc_info0.angle_out_delta) * percent;
 		float angle = arc_info0.point_angle + arc_info0.angle_out_delta;
 
-		set_val->X = arc_info0.center.X + (arc_info0.radius * ::WWMath::Sin (angle));
-		set_val->Y = arc_info0.center.Y + (arc_info0.radius * -::WWMath::Cos (angle));
+		set_val->X = arc_info0.center.X + (arc_info0.radius * ::WWMath::Sinf (angle));
+		set_val->Y = arc_info0.center.Y + (arc_info0.radius * -::WWMath::Cosf (angle));
 
 		m_Sharpness = WWMath::Clamp (WWMath::Fabsf (arc_info0.angle_out_delta) / DEG_TO_RADF (15), 0, 1.0F);
 		m_SharpnessPos.X = set_val->X;
@@ -556,13 +556,13 @@ VehicleCurveClass::Evaluate (float time, Vector3 *set_val)
 		/*float percent = 1.0F - ((seg_time - time2) / (1.0F - time2));
 		float angle = arc_info1.point_angle + (arc_info1.angle_in_delta * percent);
 
-		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sin (angle));
-		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cos (angle));			*/
+		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sinf (angle));
+		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cosf (angle));			*/
 
 		float angle = arc_info1.point_angle + (arc_info1.angle_out_delta);
 
-		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sin (angle));
-		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cos (angle));
+		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sinf (angle));
+		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cosf (angle));
 
 		m_Sharpness = WWMath::Clamp (WWMath::Fabsf (arc_info1.angle_out_delta) / DEG_TO_RADF (15), 0, 1.0F);
 		m_SharpnessPos.X = set_val->X;

Generals/Code/Tools/WorldBuilder/src/GlobalLightOptions.cpp
@@ -54,8 +54,8 @@ static void calcNewLight(Int lr, Int fb, Vector3 *newLight)
 	newLight->Set(0,0,-1);
 	Real yAngle = PI*(lr-90)/180;
 	Real xAngle = PI*(fb-90)/180;
-	Real zAngle = xAngle * WWMath::Sin(yAngle);
-	xAngle *= WWMath::Cos(yAngle);
+	Real zAngle = xAngle * WWMath::Sinf(yAngle);
+	xAngle *= WWMath::Cosf(yAngle);
 	newLight->Rotate_Y(yAngle);
 	newLight->Rotate_X(xAngle);
 	newLight->Rotate_Z(zAngle);
@@ -94,8 +94,8 @@ void GlobalLightOptions::updateEditFields()
 void GlobalLightOptions::showLightFeedback(Int lightIndex)
 {
 	Vector3 light(0,0,0);
-	light.X = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*cos(m_angleAzimuth[lightIndex]/180.0f*PI);// -WWMath::Sin(PI*(m_angleLR[lightIndex]-90)/180);
-	light.Y = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*sin(m_angleAzimuth[lightIndex]/180.0f*PI);//-WWMath::Sin(PI*(m_angleFB[lightIndex]-90)/180);
+	light.X = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*cos(m_angleAzimuth[lightIndex]/180.0f*PI);// -WWMath::Sinf(PI*(m_angleLR[lightIndex]-90)/180);
+	light.Y = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*sin(m_angleAzimuth[lightIndex]/180.0f*PI);//-WWMath::Sinf(PI*(m_angleFB[lightIndex]-90)/180);
 	light.Z = cos (PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI);
 
 	WbView3d * pView = CWorldBuilderDoc::GetActive3DView();
@@ -109,8 +109,8 @@ void GlobalLightOptions::showLightFeedback(Int lightIndex)
 void GlobalLightOptions::applyAngle(Int lightIndex)
 {
 	Vector3 light(0,0,0);
-	light.X = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*cos(m_angleAzimuth[lightIndex]/180.0f*PI);// -WWMath::Sin(PI*(m_angleLR[lightIndex]-90)/180);
-	light.Y = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*sin(m_angleAzimuth[lightIndex]/180.0f*PI);//-WWMath::Sin(PI*(m_angleFB[lightIndex]-90)/180);
+	light.X = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*cos(m_angleAzimuth[lightIndex]/180.0f*PI);// -WWMath::Sinf(PI*(m_angleLR[lightIndex]-90)/180);
+	light.Y = sin(PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI)*sin(m_angleAzimuth[lightIndex]/180.0f*PI);//-WWMath::Sinf(PI*(m_angleFB[lightIndex]-90)/180);
 	light.Z = cos (PI/2.0f+m_angleElevation[lightIndex]/180.0f*PI);
 
 	CString str;

bobtista/GeneralsGameCode

Core/Libraries/Source/WWVegas/WWMath/vehiclecurve.cpp
@@ -206,8 +206,8 @@ void Find_Turn_Arc(
 	//
 	//	Find the point on the circle at this angle
 	//
-	arc_center->X = curr_pt.X + (radius * ::WWMath::Cos(avg_angle));
-	arc_center->Y = curr_pt.Y + (radius * ::WWMath::Sin(avg_angle));
+	arc_center->X = curr_pt.X + (radius * ::WWMath::Cosf(avg_angle));
+	arc_center->Y = curr_pt.Y + (radius * ::WWMath::Sinf(avg_angle));
 	arc_center->Z = curr_pt.Z;
 
 	//
@@ -378,12 +378,12 @@ void VehicleCurveClass::Update_Arc_List()
 		//	Determine at what points these angles intersect the arc
 		//
 		Vector3 point_in(0, 0, 0);
-		point_in.X = arc_center.X + (m_Radius * ::WWMath::Sin(point_angle + angle_in_delta));
-		point_in.Y = arc_center.Y + (m_Radius * -::WWMath::Cos(point_angle + angle_in_delta));
+		point_in.X = arc_center.X + (m_Radius * ::WWMath::Sinf(point_angle + angle_in_delta));
+		point_in.Y = arc_center.Y + (m_Radius * -::WWMath::Cosf(point_angle + angle_in_delta));
 
 		Vector3 point_out(0, 0, 0);
-		point_out.X = arc_center.X + (m_Radius * ::WWMath::Sin(point_angle + angle_out_delta));
-		point_out.Y = arc_center.Y + (m_Radius * -::WWMath::Cos(point_angle + angle_out_delta));
+		point_out.X = arc_center.X + (m_Radius * ::WWMath::Sinf(point_angle + angle_out_delta));
+		point_out.Y = arc_center.Y + (m_Radius * -::WWMath::Cosf(point_angle + angle_out_delta));
 
 		//
 		//	Sanity check to ensure the vehicle doesn't try to go the long way around the
@@ -517,8 +517,8 @@ void VehicleCurveClass::Evaluate(float time, Vector3* set_val)
 		// float angle = arc_info0.point_angle + (arc_info0.angle_out_delta) * percent;
 		float angle = arc_info0.point_angle + arc_info0.angle_out_delta;
 
-		set_val->X = arc_info0.center.X + (arc_info0.radius * ::WWMath::Sin(angle));
-		set_val->Y = arc_info0.center.Y + (arc_info0.radius * -::WWMath::Cos(angle));
+		set_val->X = arc_info0.center.X + (arc_info0.radius * ::WWMath::Sinf(angle));
+		set_val->Y = arc_info0.center.Y + (arc_info0.radius * -::WWMath::Cosf(angle));
 
 		m_Sharpness = WWMath::Clamp(WWMath::Fabsf(arc_info0.angle_out_delta) / DEG_TO_RADF(15), 0, 1.0F);
 		m_SharpnessPos.X = set_val->X;
@@ -565,13 +565,13 @@ void VehicleCurveClass::Evaluate(float time, Vector3* set_val)
 		/*float percent = 1.0F - ((seg_time - time2) / (1.0F - time2));
 		float angle = arc_info1.point_angle + (arc_info1.angle_in_delta * percent);
 
-		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sin (angle));
-		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cos (angle));			*/
+		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sinf (angle));
+		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cosf (angle));			*/
 
 		float angle = arc_info1.point_angle + (arc_info1.angle_out_delta);
 
-		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sin(angle));
-		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cos(angle));
+		set_val->X = arc_info1.center.X + (arc_info1.radius * ::WWMath::Sinf(angle));
+		set_val->Y = arc_info1.center.Y + (arc_info1.radius * -::WWMath::Cosf(angle));
 
 		m_Sharpness = WWMath::Clamp(WWMath::Fabsf(arc_info1.angle_out_delta) / DEG_TO_RADF(15), 0, 1.0F);
 		m_SharpnessPos.X = set_val->X;

Generals/Code/Tools/WorldBuilder/src/GlobalLightOptions.cpp
@@ -52,8 +52,8 @@ static void calcNewLight(Int lr, Int fb, Vector3* newLight)
 	newLight->Set(0, 0, -1);
 	Real yAngle = PI * (lr - 90) / 180;
 	Real xAngle = PI * (fb - 90) / 180;
-	Real zAngle = xAngle * WWMath::Sin(yAngle);
-	xAngle *= WWMath::Cos(yAngle);
+	Real zAngle = xAngle * WWMath::Sinf(yAngle);
+	xAngle *= WWMath::Cosf(yAngle);
 	newLight->Rotate_Y(yAngle);
 	newLight->Rotate_X(xAngle);
 	newLight->Rotate_Z(zAngle);
@@ -98,8 +98,8 @@ void GlobalLightOptions::updateEditFields()
 void GlobalLightOptions::showLightFeedback(Int lightIndex)
 {
 	Vector3 light(0, 0, 0);
-	light.X = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * cos(m_angleAzimuth[lightIndex] / 180.0f * PI);    // -WWMath::Sin(PI*(m_angleLR[lightIndex]-90)/180);
-	light.Y = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * sin(m_angleAzimuth[lightIndex] / 180.0f * PI);    //-WWMath::Sin(PI*(m_angleFB[lightIndex]-90)/180);
+	light.X = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * cos(m_angleAzimuth[lightIndex] / 180.0f * PI);    // -WWMath::Sinf(PI*(m_angleLR[lightIndex]-90)/180);
+	light.Y = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * sin(m_angleAzimuth[lightIndex] / 180.0f * PI);    //-WWMath::Sinf(PI*(m_angleFB[lightIndex]-90)/180);
 	light.Z = cos(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI);
 
 	WbView3d* pView = CWorldBuilderDoc::GetActive3DView();
@@ -116,8 +116,8 @@ void GlobalLightOptions::showLightFeedback(Int lightIndex)
 void GlobalLightOptions::applyAngle(Int lightIndex)
 {
 	Vector3 light(0, 0, 0);
-	light.X = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * cos(m_angleAzimuth[lightIndex] / 180.0f * PI);    // -WWMath::Sin(PI*(m_angleLR[lightIndex]-90)/180);
-	light.Y = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * sin(m_angleAzimuth[lightIndex] / 180.0f * PI);    //-WWMath::Sin(PI*(m_angleFB[lightIndex]-90)/180);
+	light.X = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * cos(m_angleAzimuth[lightIndex] / 180.0f * PI);    // -WWMath::Sinf(PI*(m_angleLR[lightIndex]-90)/180);
+	light.Y = sin(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI) * sin(m_angleAzimuth[lightIndex] / 180.0f * PI);    //-WWMath::Sinf(PI*(m_angleFB[lightIndex]-90)/180);
 	light.Z = cos(PI / 2.0f + m_angleElevation[lightIndex] / 180.0f * PI);
 
 	CString str;