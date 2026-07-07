xezon/CnC_Generals_Zero_Hour

Add coverage for cosf/sinf/tanf

Core/Libraries/Source/WWVegas/WWMath/vector3.h
@@ -708,7 +708,7 @@ WWINLINE void Vector3::Scale(const Vector3 & scale)
  *=============================================================================================*/
 WWINLINE void Vector3::Rotate_X(float angle)
 {
-	Rotate_X(sinf(angle),cosf(angle));
+	Rotate_X(WWMath::Sinf(angle),WWMath::Cosf(angle));
 }
 
 
@@ -748,7 +748,7 @@ WWINLINE void Vector3::Rotate_X(float s_angle,float c_angle)
  *=============================================================================================*/
 WWINLINE void Vector3::Rotate_Y(float angle)
 {
-	Rotate_Y(sinf(angle),cosf(angle));
+	Rotate_Y(WWMath::Sinf(angle),WWMath::Cosf(angle));
 }
 
 
@@ -788,7 +788,7 @@ WWINLINE void Vector3::Rotate_Y(float s_angle,float c_angle)
  *=============================================================================================*/
 WWINLINE void Vector3::Rotate_Z(float angle)
 {
-	Rotate_Z(sinf(angle),cosf(angle));
+	Rotate_Z(WWMath::Sinf(angle),WWMath::Cosf(angle));
 }
 
 

bobtista/GeneralsGameCode

Core/Libraries/Source/WWVegas/WWMath/vector3.h
@@ -773,7 +773,7 @@ WWINLINE void Vector3::Scale(const Vector3& scale)
  *=============================================================================================*/
 WWINLINE void Vector3::Rotate_X(float angle)
 {
-	Rotate_X(sinf(angle), cosf(angle));
+	Rotate_X(WWMath::Sinf(angle), WWMath::Cosf(angle));
 }
 
 /***********************************************************************************************
@@ -811,7 +811,7 @@ WWINLINE void Vector3::Rotate_X(float s_angle, float c_angle)
  *=============================================================================================*/
 WWINLINE void Vector3::Rotate_Y(float angle)
 {
-	Rotate_Y(sinf(angle), cosf(angle));
+	Rotate_Y(WWMath::Sinf(angle), WWMath::Cosf(angle));
 }
 
 /***********************************************************************************************
@@ -849,7 +849,7 @@ WWINLINE void Vector3::Rotate_Y(float s_angle, float c_angle)
  *=============================================================================================*/
 WWINLINE void Vector3::Rotate_Z(float angle)
 {
-	Rotate_Z(sinf(angle), cosf(angle));
+	Rotate_Z(WWMath::Sinf(angle), WWMath::Cosf(angle));
 }
 
 /***********************************************************************************************