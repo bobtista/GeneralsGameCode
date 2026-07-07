xezon/CnC_Generals_Zero_Hour

Add coverage for sqrt

Core/Libraries/Source/WWVegas/WWMath/euler.cpp
@@ -180,7 +180,7 @@ void EulerAnglesClass::From_Matrix(const Matrix3D & M, int order)
 	_euler_unpack_order(order,i,j,k,h,n,s,f);
 
 	if (s == EULER_REPEAT_YES) {
-		double sy = sqrt(M[i][j]*M[i][j] + M[i][k]*M[i][k]);
+		double sy = WWMath::Sqrt(M[i][j]*M[i][j] + M[i][k]*M[i][k]);
 
 		if (sy > 16*FLT_EPSILON) {
 
@@ -197,7 +197,7 @@ void EulerAnglesClass::From_Matrix(const Matrix3D & M, int order)
 
 	} else {
 
-		double cy = sqrt(M[i][i]*M[i][i] + M[j][i]*M[j][i]);
+		double cy = WWMath::Sqrt(M[i][i]*M[i][i] + M[j][i]*M[j][i]);
 
 		if (cy > 16*FLT_EPSILON) {
 

bobtista/GeneralsGameCode

Core/Libraries/Source/WWVegas/WWMath/euler.cpp
@@ -174,7 +174,7 @@ void EulerAnglesClass::From_Matrix(const Matrix3D& M, int order)
 
 	if (s == EULER_REPEAT_YES)
 	{
-		double sy = sqrt(M[i][j] * M[i][j] + M[i][k] * M[i][k]);
+		double sy = WWMath::Sqrt(M[i][j] * M[i][j] + M[i][k] * M[i][k]);
 
 		if (sy > 16 * FLT_EPSILON)
 		{
@@ -194,7 +194,7 @@ void EulerAnglesClass::From_Matrix(const Matrix3D& M, int order)
 	else
 	{
 
-		double cy = sqrt(M[i][i] * M[i][i] + M[j][i] * M[j][i]);
+		double cy = WWMath::Sqrt(M[i][i] * M[i][i] + M[j][i] * M[j][i]);
 
 		if (cy > 16 * FLT_EPSILON)
 		{