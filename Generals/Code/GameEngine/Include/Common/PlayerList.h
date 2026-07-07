meerzulee/GeneralsXWeb

fix(network): Pre-compute player slot indices for fast CRC network validation

Generals/Code/GameEngine/Include/Common/PlayerList.h
@@ -113,6 +113,10 @@ public:
 	*/
 	Player *findPlayerWithNameKey(NameKeyType key);
 
+	void setSlotIndex(PlayerIndex playerIndex, Byte slotIndex);
+	Byte getSlotIndex(PlayerIndex playerIndex) const;
+	void resolveSlotIndices();
+
 	/**
 		Return the "local" player (ie, the human playing the game).
 		This will never return null.
@@ -162,6 +166,7 @@ private:
 	Player				*m_local;
 	Int						m_playerCount;
 	Player				*m_players[MAX_PLAYER_COUNT];
+	Byte					m_slotIndices[MAX_PLAYER_COUNT];
 
 };
 

Generals/Code/GameEngine/Source/Common/RTS/PlayerList.cpp
@@ -59,7 +59,7 @@
 #endif
 #include "GameLogic/SidesList.h"
 #include "GameNetwork/NetworkDefs.h"
-
+#include "GameNetwork/GameInfo.h"
 
 //-----------------------------------------------------------------------------
 /*extern*/ PlayerList *ThePlayerList = nullptr;
@@ -109,6 +109,46 @@ Player *PlayerList::findPlayerWithNameKey(NameKeyType key)
 	}
 	return nullptr;
 }
+
+//-----------------------------------------------------------------------------
+void PlayerList::setSlotIndex(PlayerIndex playerIndex, Byte slotIndex)
+{
+	if (playerIndex >= 0 && playerIndex < ARRAY_SIZE(m_slotIndices))
+	{
+		m_slotIndices[playerIndex] = slotIndex;
+	}
+}
+
+Byte PlayerList::getSlotIndex(PlayerIndex playerIndex) const
+{
+	if (playerIndex >= 0 && playerIndex < ARRAY_SIZE(m_slotIndices))
+	{
+		return m_slotIndices[playerIndex];
+	}
+
+	return -1;
+}
+
+void PlayerList::resolveSlotIndices()
+{
+	AsciiString playerName;
+
+	for (Int i = 0; i < MAX_SLOTS; ++i)
+	{
+		const GameSlot* slot = TheGameInfo->getSlot(i);
+		if (!slot || !slot->isOccupied())
+			continue;
+
+		playerName.format("player%d", i);
+
+		Player* player = findPlayerWithNameKey(TheNameKeyGenerator->nameToKey(playerName));
+		if (player)
+		{
+			setSlotIndex(player->getPlayerIndex(), i);
+		}
+	}
+}
+
 
 //-----------------------------------------------------------------------------
 void PlayerList::reset()
@@ -226,6 +266,7 @@ void PlayerList::newGame()
 		p->setDefaultTeam();
 	}
 
+	resolveSlotIndices();
 }
 
 //-----------------------------------------------------------------------------
@@ -233,6 +274,7 @@ void PlayerList::init()
 {
 	m_playerCount = 1;
 	m_players[0]->init(nullptr);
+	memset(m_slotIndices, -1, sizeof(m_slotIndices));
 
 	for (int i = 1; i < MAX_PLAYER_COUNT; i++)
 		m_players[i]->init(nullptr);

bobtista/GeneralsGameCode

Generals/Code/GameEngine/Include/Common/PlayerList.h
@@ -115,6 +115,10 @@ public:
 	*/
 	Player* findPlayerWithNameKey(NameKeyType key);
 
+	void setSlotIndex(Int playerIndex, Byte slotIndex);
+	Byte getSlotIndex(Int playerIndex) const;
+	void resolveSlotIndices();
+
 	/**
 	  Return the "local" player (ie, the human playing the game).
 	  This will never return null.
@@ -166,6 +170,7 @@ private:
 	Player* m_local;
 	Int m_playerCount;
 	Player* m_players[MAX_PLAYER_COUNT];
+	Byte m_slotIndices[MAX_PLAYER_COUNT];
 };
 
 // ----------------------------------------------------------------------------------------------

Generals/Code/GameEngine/Source/Common/RTS/PlayerList.cpp
@@ -59,6 +59,7 @@
 #endif
 #include "GameLogic/SidesList.h"
 #include "GameNetwork/NetworkDefs.h"
+#include "GameNetwork/GameInfo.h"
 
 //-----------------------------------------------------------------------------
 /*extern*/ PlayerList* ThePlayerList = nullptr;
@@ -107,6 +108,48 @@ Player* PlayerList::findPlayerWithNameKey(NameKeyType key)
 		}
 	}
 	return nullptr;
+}
+
+//-----------------------------------------------------------------------------
+void PlayerList::setSlotIndex(Int playerIndex, Byte slotIndex)
+{
+	if (playerIndex >= 0 && playerIndex < ARRAY_SIZE(m_slotIndices))
+	{
+		m_slotIndices[playerIndex] = slotIndex;
+	}
+}
+
+Byte PlayerList::getSlotIndex(Int playerIndex) const
+{
+	if (playerIndex >= 0 && playerIndex < ARRAY_SIZE(m_slotIndices))
+	{
+		return m_slotIndices[playerIndex];
+	}
+
+	return -1;
+}
+
+void PlayerList::resolveSlotIndices()
+{
+	if (!TheGameInfo)
+		return;
+
+	AsciiString playerName;
+
+	for (Int i = 0; i < MAX_SLOTS; ++i)
+	{
+		const GameSlot* slot = TheGameInfo->getSlot(i);
+		if (!slot || !slot->isOccupied())
+			continue;
+
+		playerName.format("player%d", i);
+
+		Player* player = findPlayerWithNameKey(TheNameKeyGenerator->nameToKey(playerName));
+		if (player)
+		{
+			setSlotIndex(player->getPlayerIndex(), i);
+		}
+	}
 }
 
 //-----------------------------------------------------------------------------
@@ -224,6 +267,8 @@ void PlayerList::newGame()
 
 		p->setDefaultTeam();
 	}
+
+	resolveSlotIndices();
 }
 
 //-----------------------------------------------------------------------------
@@ -231,6 +276,7 @@ void PlayerList::init()
 {
 	m_playerCount = 1;
 	m_players[0]->init(nullptr);
+	memset(m_slotIndices, -1, sizeof(m_slotIndices));
 
 	for (int i = 1; i < MAX_PLAYER_COUNT; i++)
 		m_players[i]->init(nullptr);