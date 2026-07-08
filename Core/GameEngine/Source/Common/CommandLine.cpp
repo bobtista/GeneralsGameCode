791f631fe4cbe171ed36aeac4e9e6c0c9758daf7
Bobby Battista <bobtista@gmail.com>
2026-07-08 16:03:25 +0400

    tweak: drop dead -bgfxInstancing and -bgfxKeepStaticVolumeShadows arguments

--- Core/GameEngine/Source/Common/CommandLine.cpp
+++ Core/GameEngine/Source/Common/CommandLine.cpp
@@ -227,11 +227,6 @@ Int parseBgfxProbeTransposeInstances(char *args[], int)
 	return parseSetEnvFlag("GGC_PROBE_TRANSPOSE_INSTANCES");
 }
 
-Int parseBgfxInstancing(char *args[], int)
-{
-	return parseSetEnvFlag("GGC_BGFX_INSTANCING");
-}
-
 Int parseBgfxInstancingNoReorder(char *args[], int)
 {
 	return parseSetEnvFlag("GGC_BGFX_INSTANCING_NO_REORDER");
@@ -1372,12 +1367,6 @@ Int parseBgfxSkipStaticVolumeShadows(char *args[], int)
 	return 1;
 }
 
-Int parseBgfxKeepStaticVolumeShadows(char *args[], int)
-{
-	setenv("GGC_BGFX_KEEP_STATIC_VOLUME_SHADOWS", "1", 1);
-	return 1;
-}
-
 Int parseBgfxNoSceneFramebuffer(char *args[], int num)
 {
 	TheWritableGlobalData->m_bgfxNoSceneFramebuffer = TRUE;
@@ -1580,7 +1569,6 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-fixedLogicFPS", parseFixedLogicFPS },
 	{ "-perfAutoExitSeconds", parsePerfAutoExitSeconds },
 	{ "-bgfxSkipStaticVolumeShadows", parseBgfxSkipStaticVolumeShadows },
-	{ "-bgfxKeepStaticVolumeShadows", parseBgfxKeepStaticVolumeShadows },
 	{ "-bgfxProbeNullSubmit", parseBgfxProbeNullSubmit },
 	{ "-bgfxProbeFreezeState", parseBgfxProbeFreezeState },
 	{ "-bgfxProbeNoSorted", parseBgfxProbeNoSorted },
@@ -1593,7 +1581,6 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-bgfxProbeNoSceneObjectRender", parseBgfxProbeNoSceneObjectRender },
 	{ "-bgfxProbeIdentityInstances", parseBgfxProbeIdentityInstances },
 	{ "-bgfxProbeTransposeInstances", parseBgfxProbeTransposeInstances },
-	{ "-bgfxInstancing", parseBgfxInstancing },
 	{ "-bgfxInstancingNoReorder", parseBgfxInstancingNoReorder },
 	{ "-bgfxDisableInstancing", parseBgfxDisableInstancing },
 	{ "-bgfxDisableSortedMaterialRecaptureSkip", parseBgfxDisableSortedMaterialRecaptureSkip },
