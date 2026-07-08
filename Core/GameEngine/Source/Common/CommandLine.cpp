e5a36ca1e64482e13a06cbb47d6d700663722254
Bobby Battista <bobtista@gmail.com>
2026-07-08 19:44:34 +0400

    feat: add -bgfxSortedTextureArray argument for the sorted texture-array merge path

--- Core/GameEngine/Source/Common/CommandLine.cpp
+++ Core/GameEngine/Source/Common/CommandLine.cpp
@@ -240,6 +240,11 @@ Int parseBgfxDisableSortedMaterialSnapshot(char *args[], int)
 	return parseSetFlag(GgcFlag_BgfxDisableSortedMaterialSnapshot);
 }
 
+Int parseBgfxSortedTextureArray(char *args[], int)
+{
+	return parseSetFlag(GgcFlag_BgfxSortedTextureArray);
+}
+
 Int parseBgfxFrameTimingAfter(char *args[], int num)
 {
 	if (num > 1)
@@ -1602,6 +1607,7 @@ static CommandLineParam paramsForEngineInit[] =
 	{ "-bgfxDisableInstancing", parseBgfxDisableInstancing },
 	{ "-bgfxDisableSortedMaterialRecaptureSkip", parseBgfxDisableSortedMaterialRecaptureSkip },
 	{ "-bgfxDisableSortedMaterialSnapshot", parseBgfxDisableSortedMaterialSnapshot },
+	{ "-bgfxSortedTextureArray", parseBgfxSortedTextureArray },
 	{ "-bgfxFrameTimingAfter", parseBgfxFrameTimingAfter },
 	{ "-bgfxFrameTimingInterval", parseBgfxFrameTimingInterval },
 	{ "-bgfxFrameTimingPath", parseBgfxFrameTimingPath },
