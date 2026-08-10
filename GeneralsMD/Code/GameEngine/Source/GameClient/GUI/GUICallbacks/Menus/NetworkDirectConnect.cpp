ab07bb46300f76de7bc191c46205bdfff8c1a0c4
Bobby Battista <bobtista@gmail.com>
2026-08-10 12:52:11 -0400

    feat(network): Add debug command-line match startup

--- GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/NetworkDirectConnect.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/NetworkDirectConnect.cpp
@@ -49,6 +49,7 @@
 #include "GameNetwork/IPEnumeration.h"
 #include "GameNetwork/LANAPI.h"
 #include "GameNetwork/LANAPICallbacks.h"
+#include "GameNetwork/NetworkAutoStart.h"
 
 
 // window ids ------------------------------------------------------------------------------
@@ -257,9 +258,17 @@ void NetworkDirectConnectInit( WindowLayout *layout, void *userData )
 	LANbuttonPushed = false;
 	LANisShuttingDown = false;
 
+	UnsignedInt autoLocalIP = 0;
+#if defined(RTS_DEBUG)
+	if (NetworkAutoStart::isEnabled())
+		autoLocalIP = NetworkAutoStart::getLocalAddress();
+#endif
+
 	if (TheLAN == nullptr)
 	{
 		TheLAN = NEW LANAPI();
+		if (autoLocalIP != 0)
+			TheLAN->SetLocalIP(autoLocalIP);
 		TheLAN->init();
 	}
 	TheLAN->reset();
@@ -310,6 +319,8 @@ void NetworkDirectConnectInit( WindowLayout *layout, void *userData )
 
 		OptionPreferences prefs;
 		UnsignedInt IP = prefs.getOnlineIPAddress();
+		if (autoLocalIP != 0)
+			IP = autoLocalIP;
 
 		IPEnumeration IPs;
 
@@ -322,7 +333,7 @@ void NetworkDirectConnectInit( WindowLayout *layout, void *userData )
 				/// @todo: display error and exit lan lobby if no IPs are found
 			}
 
-			Bool foundIP = FALSE;
+			Bool foundIP = autoLocalIP != 0;
 			EnumeratedIP *tempIP = IPlist;
 			while ((tempIP != nullptr) && (foundIP == FALSE)) {
 				if (IP == tempIP->getIP()) {
@@ -338,8 +349,16 @@ void NetworkDirectConnectInit( WindowLayout *layout, void *userData )
 
 //			IP = IPlist->getIP();
 //		}
-		TheLAN->init();
-		TheLAN->SetLocalIP(IP);
+		if (autoLocalIP != 0)
+		{
+			TheLAN->SetLocalIP(autoLocalIP);
+			TheLAN->init();
+		}
+		else
+		{
+			TheLAN->init();
+			TheLAN->SetLocalIP(IP);
+		}
 	}
 
 	UnsignedInt ip = TheLAN->GetLocalIP();
@@ -398,6 +417,14 @@ void NetworkDirectConnectShutdown( WindowLayout *layout, void *userData )
 //-------------------------------------------------------------------------------------------------
 void NetworkDirectConnectUpdate( WindowLayout * layout, void *userData)
 {
+#if defined(RTS_DEBUG)
+	if (NetworkAutoStart::isEnabled() && TheLAN != nullptr)
+	{
+		TheLAN->update();
+		NetworkAutoStart::updateDirectConnect();
+	}
+#endif
+
 	// We'll only be successful if we've requested to
 	if(isShuttingDown && TheShell->isAnimFinished() && TheTransitionHandler->isFinished())
 		shutdownComplete(layout);
