7f5465ae5e8558dbf761ca411c9f62b8192289e8
Bobby Battista <bobtista@gmail.com>
2026-08-13 19:15:08 -0400

    bugfix(saveload): Restore dock approach position reached flags on load (Generals)

--- Generals/Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/DockUpdate.cpp
+++ Generals/Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/DockUpdate.cpp
@@ -600,8 +600,10 @@ void DockUpdate::xfer( Xfer *xfer )
 	for( vectorIndex = 0; vectorIndex < vectorSize; ++vectorIndex )
 	{
 		// Vector of Bool gets packed as bitfield internally
+		// TheSuperHackers @fix bobtista 12/08/2026 Write the unpacked value back so the load takes effect
 		Bool unpack = m_approachPositionReached[vectorIndex];
 		xfer->xferBool( &unpack );
+		m_approachPositionReached[vectorIndex] = unpack;
 	}
 
 	// active docker
