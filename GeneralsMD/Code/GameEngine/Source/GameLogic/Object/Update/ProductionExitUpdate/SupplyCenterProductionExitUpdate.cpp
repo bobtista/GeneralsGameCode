9be7ee301627d2c10be083417b35435ea9acffec
Bobby Battista <bobtista@gmail.com>
2026-08-17 00:02:45 -0400

    bugfix(productionexit): Derive the exit heading from the producer transform
    (cherry picked from commit 3023e84366a604f1ce3653e600312a59bdb44f93)

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Update/ProductionExitUpdate/SupplyCenterProductionExitUpdate.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Update/ProductionExitUpdate/SupplyCenterProductionExitUpdate.cpp
@@ -66,8 +66,11 @@ void SupplyCenterProductionExitUpdate::exitObjectViaDoor( Object *newObj, ExitDo
 	{
 		const SupplyCenterProductionExitUpdateModuleData* md = getSupplyCenterProductionExitUpdateModuleData();
 
-		Real exitAngle = creationObject->getOrientation();
+		// TheSuperHackers @bugfix bobtista 16/08/2026 Take the exit heading from the transform the
+		// frame CRC covers rather than the cached orientation angle. The two can disagree by one ULP,
+		// which puts a history dependent heading on the produced object.
 		const Matrix3D *transform = creationObject->getTransformMatrix();
+		Real exitAngle = transform->Get_Z_Rotation();
 		Vector3 loc;
 		Coord3D createPoint;
 
