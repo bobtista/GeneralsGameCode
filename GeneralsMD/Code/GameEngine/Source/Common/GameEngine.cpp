397e7fd56a93ae1031d3373b14c4415c610b7632
Bobby Battista <bobtista@gmail.com>
2026-09-08 08:34:48 -0500

    Revert "bugfix(network): Take the next recovery peer as soon as the current transfer completes"
    This reverts commit c2f9caf37c341df18a40fd7de77a98046cbf1248.

--- GeneralsMD/Code/GameEngine/Source/Common/GameEngine.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/GameEngine.cpp
@@ -981,10 +981,7 @@ void GameEngine::update()
 								s_lastTransferPercent = nowPercent;
 								s_lastTransferProgressTime = now;
 							}
-							// A peer sitting at 0% is queued behind another transfer, not stalled
-							// mid-stream, so it may ask again at the fast cadence.
-							else if ((UnsignedInt)(now - s_lastTransferProgressTime) >=
-								((nowPercent > 0) ? (UnsignedInt)RECOVERY_TRANSFER_STALL_MS : (UnsignedInt)REJOIN_REQUEST_INTERVAL_MS))
+							else if ((UnsignedInt)(now - s_lastTransferProgressTime) >= (UnsignedInt)RECOVERY_TRANSFER_STALL_MS)
 							{
 								s_lastTransferProgressTime = now;
 								TheNetwork->sendRejoinRequest();
