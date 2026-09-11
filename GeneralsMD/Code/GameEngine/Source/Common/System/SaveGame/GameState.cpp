9b78f615ba8f43aedcba3c171af9a87fdcf5a142
Bobby Battista <bobtista@gmail.com>
2026-09-11 10:08:11 -0500

    tweak(saveload): Keep the retail load path for user saves and apply the load guards to checkpoints only

--- GeneralsMD/Code/GameEngine/Source/Common/System/SaveGame/GameState.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/System/SaveGame/GameState.cpp
@@ -2019,7 +2019,15 @@ void GameState::gameStatePostProcessLoad()
 	// evil... must ensure this is updated prior to the script engine running the first time.
 	phaseStart = timeGetTime();
 	TheGameLogic->updateLoadProgress( 99 );
-	ThePartitionManager->updateCellsOnlyForLoad();
+	const Bool checkpointLoad = getSaveGameInfo()->saveFileType == SAVE_FILE_TYPE_CHECKPOINT;
+	if( checkpointLoad )
+	{
+		ThePartitionManager->updateCellsOnlyForLoad();
+	}
+	else
+	{
+		ThePartitionManager->update();
+	}
 	ThePartitionManager->finishLoadPostProcess();
 	// Objects saved while owing a partition update get it back now, after the load-time cell pass.
 	for( Object *obj = TheGameLogic->getFirstObject(); obj != nullptr; obj = obj->getNextObject() )
@@ -2038,7 +2046,10 @@ void GameState::gameStatePostProcessLoad()
 	}
 	DEBUG_LOG(("gameStatePostProcessLoad: partition finish took %d ms", timeGetTime() - phaseStart));
 
-	TheVictoryConditions->resyncDefeatStateAfterLoad();
+	if( checkpointLoad )
+	{
+		TheVictoryConditions->resyncDefeatStateAfterLoad();
+	}
 
 }
 
