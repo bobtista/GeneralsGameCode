b421c258f709228fba23e854a9d99c3e99b5eea3
Bobby Battista <bobtista@gmail.com>
2026-09-11 10:04:11 -0500

    bugfix(script): Suppress scripted game ends only during a playback resumed from a checkpoint

--- GeneralsMD/Code/GameEngine/Include/Common/Recorder.h
+++ GeneralsMD/Code/GameEngine/Include/Common/Recorder.h
@@ -140,6 +140,7 @@ public:
 	RecorderModeType getMode();												///< Returns the current operating mode.
 	Bool isPlaybackMode() const { return m_mode == RECORDERMODETYPE_PLAYBACK || m_mode == RECORDERMODETYPE_SIMULATION_PLAYBACK; }
 	Bool resumePlayback( AsciiString filename, UnsignedInt frame );	///< Re-enter playback of a replay from a checkpoint frame: skip commands at or before it.
+	Bool isResumedPlayback() const { return m_resumedPlayback; }		///< True while a playback entered through resumePlayback is running.
 	void initControls();															///< Show or Hide the Replay controls
 
 	static AsciiString getReplayDir();								///< Returns the directory that holds the replay files.
@@ -200,8 +201,9 @@ protected:
 	Int m_originalGameMode; // valid in replays
 
 	UnsignedInt m_nextFrame;												///< The Frame that the next message is to be executed on.  This can be -1.
-	Bool m_resumeSkipCommands;
-	UnsignedInt m_resumeMinCRCFrame;								///< Recorded CRCs describing frames before this are stale after a checkpoint resume.											///< True while resumePlayback discards commands up to the checkpoint frame.
+	Bool m_resumeSkipCommands;											///< True while resumePlayback discards commands up to the checkpoint frame.
+	UnsignedInt m_resumeMinCRCFrame;								///< Recorded CRCs describing frames before this are stale after a checkpoint resume.
+	Bool m_resumedPlayback;													///< The current playback was entered from a checkpoint.
 };
 
 extern RecorderClass *TheRecorder;
--- GeneralsMD/Code/GameEngine/Source/Common/Recorder.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/Recorder.cpp
@@ -348,6 +348,7 @@ RecorderClass::RecorderClass()
 	m_nextFrame = 0;
 	m_resumeSkipCommands = FALSE;
 	m_resumeMinCRCFrame = 0;
+	m_resumedPlayback = FALSE;
 	m_wasDesync = FALSE;
 	init(); // just for the heck of it.
 }
@@ -383,6 +384,7 @@ void RecorderClass::init() {
 	m_playbackFrameCount = 0;
 	m_resumeSkipCommands = FALSE;
 	m_resumeMinCRCFrame = 0;
+	m_resumedPlayback = FALSE;
 
 	OptionPreferences optionPref;
 	m_archiveReplays = optionPref.getArchiveReplaysEnabled();
@@ -1360,6 +1362,7 @@ Bool RecorderClass::resumePlayback( AsciiString filename, UnsignedInt frame )
 	m_resumeSkipCommands = FALSE;
 
 	m_resumeMinCRCFrame = frame;
+	m_resumedPlayback = TRUE;
 	m_mode = RECORDERMODETYPE_PLAYBACK;
 	m_currentReplayFilename = filename;
 	m_playbackFrameCount = header.frameCount;
