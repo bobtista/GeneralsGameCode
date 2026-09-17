b253fdce65fcbacd090b3ac41c08f2afe9c3d598
Bobby Battista <bobtista@gmail.com>
2026-09-16 23:59:05 -0400

    build(cli): Register the save frame options outside debug builds

--- GeneralsMD/Code/GameEngine/Source/Common/GlobalData.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/GlobalData.cpp
@@ -1557,10 +1557,8 @@ GlobalData::GlobalData()
 
 	m_buildMapCache = FALSE;
 	m_initialFile.clear();
-#if defined(RTS_DEBUG)
 	m_saveAtFrame = 0;
 	m_saveToFile = "commandline.sav";
-#endif
 	m_pendingFile.clear();
 	m_loadSaveGame.clear();
 	m_loadReplayGame.clear();
--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
@@ -4136,7 +4136,6 @@ void GameLogic::update()
 
 	PROFILER_PLOT("LogicFrame", static_cast<int64_t>(now));
 
-#if defined(RTS_DEBUG)
 	// Save before scripts and object updates so loading does not repeat part of a frame.
 	if (TheGlobalData->m_saveAtFrame > 0 && m_frame >= static_cast<UnsignedInt>(TheGlobalData->m_saveAtFrame)
 		&& isInInteractiveGame())
@@ -4168,7 +4167,6 @@ void GameLogic::update()
 			return;
 		}
 	}
-#endif
 
 	// update (execute) scripts
 	{
