4d657151d9f058cd418c8767e69c11046abcdec4
Bobby Battista <bobtista@gmail.com>
2026-09-21 11:45:28 -0400

    bugfix(pathfinder): Switch to the fixed pathfinding when a move away order destroys the path under the moveAllies walk

--- Core/GameEngine/Include/GameLogic/AIPathfind.h
+++ Core/GameEngine/Include/GameLogic/AIPathfind.h
@@ -869,6 +869,8 @@ protected:
 
 	bool checkCellOutsideExtents(ICoord2D& cell);
 
+	void moveAlliesAroundCell(Object *obj, const ICoord2D &curCell, PathfindLayerEnum layer, Int radius, Int numCellsAbove, ObjectID ignoreId, Bool blockedByAlly);	///< Order the idle allies around one path cell out of the way
+
 #if defined(RTS_DEBUG)
 	void doDebugIcons() ;
 #endif
@@ -902,7 +904,6 @@ private:
 	Real					m_wallHeight;
 
 	Int						m_moveAlliesDepth;
-#if !RETAIL_COMPATIBLE_PATHFINDING
 	struct MoveAlliesCell
 	{
 		ICoord2D cell;
@@ -910,7 +911,6 @@ private:
 	};
 	enum { MOVE_ALLIES_MAX_DEPTH = 3 };
 	std::vector<MoveAlliesCell> m_moveAlliesCells[MOVE_ALLIES_MAX_DEPTH];	///< one per recursion depth, moveAllies nests
-#endif
 
 
 	// Pathfind queue
