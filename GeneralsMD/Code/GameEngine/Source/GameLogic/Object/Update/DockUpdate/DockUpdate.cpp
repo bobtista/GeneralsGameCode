a1c545ea3f1cd26b9830eb40522fcea023df8624
Bobby Battista <bobtista@gmail.com>
2026-09-22 16:00:35 -0400

    chore(saveload): Remove redundant dock flag comment

--- GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/DockUpdate.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/DockUpdate.cpp
@@ -615,7 +615,6 @@ void DockUpdate::xfer( Xfer *xfer )
 	for( vectorIndex = 0; vectorIndex < vectorSize; ++vectorIndex )
 	{
 		// Vector of Bool gets packed as bitfield internally
-		// TheSuperHackers @fix bobtista 12/08/2026 Restore saved approach flags on load
 		Bool unpack = m_approachPositionReached[vectorIndex];
 		xfer->xferBool( &unpack );
 		m_approachPositionReached[vectorIndex] = unpack;
